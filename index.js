1466797eeac27f08d433df60ab6513de2aabce96
markmendelasis <markmendel.asis09@gmail.com>
2023-11-02 08:26:56 +0800

    Second Commit

--- index.js
+++ index.js
@@ -11,11 +11,29 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+const data = 
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
 app.get("/about", (req, res) => {
+    res.render("about.ejs");
+})
+
+app.get("/taylor-swift", (req, res) => {
+    res.render("taylor-swift.ejs");
+})
+
+app.get("/fearless", (req, res) => {
+    res.render("about.ejs");
+})
+
+app.get("/speak-now", (req, res) => {
+    res.render("about.ejs");
+})
+
+app.get("/red", (req, res) => {
     res.render("about.ejs");
 })
 
@@ -27,3 +45,4 @@ app.post("/submit", (req, res) => {
 app.listen(port, ()=> {
     console.log(`Server started at port ${port} `);
 });
+
---
+++ public/script.js
@@ -0,0 +1,146 @@
+// ADDEventlistener
+
+// Taylor First Album
+var selfTitledAlbum = document.getElementById("selfTitled");
+
+selfTitledAlbum.addEventListener("mouseenter", function(){
+    selfTitledAlbum.style.transform = "scale(1.5)"
+    selfTitledAlbum.style.marginBottom = ("2vh")
+    
+    
+}); 
+selfTitledAlbum.addEventListener("mouseleave", function(){
+    selfTitledAlbum.style.transform = "scale(1)"
+    selfTitledAlbum.style.marginBottom = ("0")
+    
+});
+
+// Fearless
+var fearless = document.getElementById("fearless");
+
+fearless.addEventListener("mouseenter", function(){
+    fearless.style.transform = "scale(1.5)"
+    fearless.style.marginBottom = ("2vh")
+}); 
+fearless.addEventListener("mouseleave", function(){
+    fearless.style.transform = "scale(1)"
+    fearless.style.marginBottom = ("0")
+});
+
+
+// Speaknow
+
+var speakNow = document.getElementById("speakNow");
+
+speakNow.addEventListener("mouseenter", function(){
+    speakNow.style.transform = "scale(1.5)"
+    speakNow.style.marginBottom = ("2vh")
+}); 
+speakNow.addEventListener("mouseleave", function(){
+    speakNow.style.transform = "scale(1)"
+    speakNow.style.marginBottom = ("0")
+});
+
+// Red
+
+var red = document.getElementById("red");
+
+red.addEventListener("mouseenter", function(){
+    red.style.transform = "scale(1.5)"
+    red.style.marginBottom = ("2vh")
+    
+}); 
+red.addEventListener("mouseleave", function(){
+    red.style.transform = "scale(1)"
+    red.style.marginBottom = ("0")
+
+});
+
+// 1989
+
+var nineteen89 = document.getElementById("nineteen89");
+
+nineteen89.addEventListener("mouseenter", function(){
+    nineteen89.style.transform = "scale(1.5)"
+    nineteen89.style.marginBottom = ("2vh")
+    
+}); 
+nineteen89.addEventListener("mouseleave", function(){
+    nineteen89.style.transform = "scale(1)"
+    nineteen89.style.marginBottom = ("0")
+
+});
+
+// Reputation
+var reputation = document.getElementById("reputation");
+
+reputation.addEventListener("mouseenter", function(){
+    reputation.style.transform = "scale(1.5)"
+    reputation.style.marginBottom = ("2vh")
+    
+}); 
+reputation.addEventListener("mouseleave", function(){
+    reputation.style.transform = "scale(1)"
+    reputation.style.marginBottom = ("0")
+
+});
+
+
+
+// Lover
+var lover = document.getElementById("lover");
+
+lover.addEventListener("mouseenter", function(){
+    lover.style.transform = "scale(1.5)"
+    lover.style.marginBottom = ("2vh")
+    
+}); 
+lover.addEventListener("mouseleave", function(){
+    lover.style.transform = "scale(1)"
+    lover.style.marginBottom = ("0")
+
+});
+
+
+// folklore
+var folklore = document.getElementById("folklore");
+
+folklore.addEventListener("mouseenter", function(){
+    folklore.style.transform = "scale(1.5)"
+    folklore.style.marginBottom = ("2vh")
+    
+}); 
+folklore.addEventListener("mouseleave", function(){
+    folklore.style.transform = "scale(1)"
+    folklore.style.marginBottom = ("0")
+
+});
+// evermore
+
+var evermore = document.getElementById("evermore");
+
+evermore.addEventListener("mouseenter", function(){
+    evermore.style.transform = "scale(1.5)"
+    evermore.style.marginBottom = ("2vh")
+    
+}); 
+evermore.addEventListener("mouseleave", function(){
+    evermore.style.transform = "scale(1)"
+    evermore.style.marginBottom = ("0")
+
+});
+
+// Midnights
+var midnights = document.getElementById("midnights");
+
+midnights.addEventListener("mouseenter", function(){
+    midnights.style.transform = "scale(1.5)"
+    midnights.style.marginBottom = ("2vh")
+    
+    
+}); 
+midnights.addEventListener("mouseleave", function(){
+    midnights.style.transform = "scale(1)"
+    midnights.style.marginBottom = ("0")
+
+});
