8d80ce431d80d9f346491f19793cc7e04a548632
markmendelasis <markmendel.asis09@gmail.com>
2023-11-06 16:51:37 +0800

    Background per album

--- public/script.js
+++ public/script.js
@@ -1,17 +1,20 @@
 // ADDEventlistener
-
+var backgroundColor = document.getElementById("main")
 // Taylor First Album
 var selfTitledAlbum = document.getElementById("selfTitled");
 
 selfTitledAlbum.addEventListener("mouseenter", function(){
     selfTitledAlbum.style.transform = "scale(1.5)";
     selfTitledAlbum.style.marginBottom = ("2vh");
+    backgroundColor.style.background = `url("https://i.insider.com/5716327f9105844e018bd968?width=948&format=jpeg")`;
+    backgroundColor.style.backgroundSize = "500px";
     
     
 }); 
 selfTitledAlbum.addEventListener("mouseleave", function(){
-    selfTitledAlbum.style.transform = "scale(1)"
-    selfTitledAlbum.style.marginBottom = ("0")
+    selfTitledAlbum.style.transform = "scale(1)";
+    selfTitledAlbum.style.marginBottom = ("0");
+    backgroundColor.style.background = `unset`;
     
 });
 
@@ -19,12 +22,15 @@ selfTitledAlbum.addEventListener("mouseleave", function(){
 var fearless = document.getElementById("fearless");
 
 fearless.addEventListener("mouseenter", function(){
-    fearless.style.transform = "scale(1.5)"
-    fearless.style.marginBottom = ("2vh")
+    fearless.style.transform = "scale(1.5)";
+    fearless.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://kwhsthesnapperorg.files.wordpress.com/2021/04/img_1914.jpg?w=961&h=768&crop=1")`;
+    backgroundColor.style.backgroundSize = "500px";
 }); 
 fearless.addEventListener("mouseleave", function(){
-    fearless.style.transform = "scale(1)"
-    fearless.style.marginBottom = ("0")
+    fearless.style.transform = "scale(1)";
+    fearless.style.marginBottom = ("0");
+    backgroundColor.style.background = `unset`;
 });
 
 
@@ -33,12 +39,15 @@ fearless.addEventListener("mouseleave", function(){
 var speakNow = document.getElementById("speakNow");
 
 speakNow.addEventListener("mouseenter", function(){
-    speakNow.style.transform = "scale(1.5)"
-    speakNow.style.marginBottom = ("2vh")
+    speakNow.style.transform = "scale(1.5)";
+    speakNow.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://squarecatvinyl.com/cdn/shop/files/Taylor-Swift-Speak-Now-Taylors-Version-cover.webp?v=1688739636")`;
+    backgroundColor.style.backgroundSize = "500px";
 }); 
 speakNow.addEventListener("mouseleave", function(){
-    speakNow.style.transform = "scale(1)"
-    speakNow.style.marginBottom = ("0")
+    speakNow.style.transform = "scale(1)";
+    speakNow.style.marginBottom = ("0");
+    backgroundColor.style.background = `unset`;
 });
 
 // Red
@@ -46,13 +55,16 @@ speakNow.addEventListener("mouseleave", function(){
 var red = document.getElementById("red");
 
 red.addEventListener("mouseenter", function(){
-    red.style.transform = "scale(1.5)"
-    red.style.marginBottom = ("2vh")
+    red.style.transform = "scale(1.5)";
+    red.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://m.media-amazon.com/images/I/61lQN5OMvpL.jpg")`;
+    backgroundColor.style.backgroundSize = "500px";
     
 }); 
 red.addEventListener("mouseleave", function(){
-    red.style.transform = "scale(1)"
-    red.style.marginBottom = ("0")
+    red.style.transform = "scale(1)";
+    red.style.marginBottom = ("0");
+    backgroundColor.style.background = `unset`;
 
 });
 
@@ -61,13 +73,16 @@ red.addEventListener("mouseleave", function(){
 var nineteen89 = document.getElementById("nineteen89");
 
 nineteen89.addEventListener("mouseenter", function(){
-    nineteen89.style.transform = "scale(1.5)"
-    nineteen89.style.marginBottom = ("2vh")
+    nineteen89.style.transform = "scale(1.5)";
+    nineteen89.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://media.pitchfork.com/photos/650de105eacc5b460e151343/master/w_1280%2Cc_limit/Taylor-Swift-1989-Taylors-Version.jpg")`;
+    backgroundColor.style.backgroundSize = "500px";
     
 }); 
 nineteen89.addEventListener("mouseleave", function(){
-    nineteen89.style.transform = "scale(1)"
-    nineteen89.style.marginBottom = ("0")
+    nineteen89.style.transform = "scale(1)";
+    nineteen89.style.marginBottom = ("0");
+    backgroundColor.style.background = `unset`;
 
 });
 
@@ -75,13 +90,16 @@ nineteen89.addEventListener("mouseleave", function(){
 var reputation = document.getElementById("reputation");
 
 reputation.addEventListener("mouseenter", function(){
-    reputation.style.transform = "scale(1.5)"
-    reputation.style.marginBottom = ("2vh")
+    reputation.style.transform = "scale(1.5)";
+    reputation.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://i.pinimg.com/originals/10/52/54/10525478f64895e19b34116e48fbb79c.jpg")`;
+    backgroundColor.style.backgroundSize = "500px";
     
 }); 
 reputation.addEventListener("mouseleave", function(){
-    reputation.style.transform = "scale(1)"
-    reputation.style.marginBottom = ("0")
+    reputation.style.transform = "scale(1)";
+    reputation.style.marginBottom = ("0");
+    backgroundColor.style.background = `unset`;
 
 });
 
@@ -91,13 +109,16 @@ reputation.addEventListener("mouseleave", function(){
 var lover = document.getElementById("lover");
 
 lover.addEventListener("mouseenter", function(){
-    lover.style.transform = "scale(1.5)"
-    lover.style.marginBottom = ("2vh")
+    lover.style.transform = "scale(1.5)";
+    lover.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/08/22/19/taylor-swift.jpg?width=1200&height=1200&fit=crop")`;
+    backgroundColor.style.backgroundSize = "500px";
     
 }); 
 lover.addEventListener("mouseleave", function(){
-    lover.style.transform = "scale(1)"
-    lover.style.marginBottom = ("0")
+    lover.style.transform = "scale(1)";
+    lover.style.marginBottom = ("0");
+    backgroundColor.style.background = `unset`;
 
 });
 
@@ -106,13 +127,15 @@ lover.addEventListener("mouseleave", function(){
 var folklore = document.getElementById("folklore");
 
 folklore.addEventListener("mouseenter", function(){
-    folklore.style.transform = "scale(1.5)"
-    folklore.style.marginBottom = ("2vh")
+    folklore.style.transform = "scale(1.5)";
+    folklore.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/31/2020/07/24144040/bec5b82900866ece18f2fe6e162ee674ed-taylor-swift-folklore.rsquare.w1200-1024x1024.jpg")`;
+    backgroundColor.style.backgroundSize = "500px";
     
 }); 
 folklore.addEventListener("mouseleave", function(){
-    folklore.style.transform = "scale(1)"
-    folklore.style.marginBottom = ("0")
+    folklore.style.transform = "scale(1)";
+    folklore.style.marginBottom = ("0");
 
 });
 // evermore
@@ -120,13 +143,15 @@ folklore.addEventListener("mouseleave", function(){
 var evermore = document.getElementById("evermore");
 
 evermore.addEventListener("mouseenter", function(){
-    evermore.style.transform = "scale(1.5)"
-    evermore.style.marginBottom = ("2vh")
+    evermore.style.transform = "scale(1.5)";
+    evermore.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://i.redd.it/oy1ieq961zd61.jpg")`;
+    backgroundColor.style.backgroundSize = "500px";
     
 }); 
 evermore.addEventListener("mouseleave", function(){
-    evermore.style.transform = "scale(1)"
-    evermore.style.marginBottom = ("0")
+    evermore.style.transform = "scale(1)";
+    evermore.style.marginBottom = ("0");
 
 });
 
@@ -134,13 +159,16 @@ evermore.addEventListener("mouseleave", function(){
 var midnights = document.getElementById("midnights");
 
 midnights.addEventListener("mouseenter", function(){
-    midnights.style.transform = "scale(1.5)"
-    midnights.style.marginBottom = ("2vh")
+    midnights.style.transform = "scale(1.5)";
+    midnights.style.marginBottom = ("2vh");
+    backgroundColor.style.background =`url("https://www.rmusentrymedia.com/wp-content/uploads/2022/10/Midnights.jpg")`;
+    backgroundColor.style.backgroundSize = "500px";
     
     
 }); 
 midnights.addEventListener("mouseleave", function(){
-    midnights.style.transform = "scale(1)"
-    midnights.style.marginBottom = ("0")
+    midnights.style.transform = "scale(1)";
+    midnights.style.marginBottom = ("0");
+    backgroundColor.style.background = `unset`;
 
 });
